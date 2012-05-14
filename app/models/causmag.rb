37ed3e7b312df3694fc7021bb6f15e8cf76550e9
unknown <Prod01@Prod01-HP.(none)>
2012-05-14 16:42:02 +0200

    cambiato modello Causmag secondo nuove specifiche

--- app/models/causmag.rb
+++ app/models/causmag.rb
@@ -1,9 +1,10 @@
 class Causmag < ActiveRecord::Base
   has_many :tesdocs
 
-  attr_accessible :azienda, :descriz, :contabile, :tipo, :movimpmag
+  attr_accessible :azienda, :tipo_doc, :descriz, :des_caus, :tipo, :movimpmag, :nrmag_src, :nrmag_dst, :contabile, :modulo
   validates :azienda, :descriz, :contabile, :tipo, :movimpmag, :presence => true
   # Fare un validate con i valori ammessi
-  validates :contabile, :length => { :maximum => 1, :too_long  => "1 carattere obbligatorio (Valori ammessi: S = Mov.Contabile - N = Mov.non Contabile" }
+  validates :descriz, :des_caus, :length => { :maximum => 100, :too_long  => "Lunghezza massima consentita: 100 caratteri" }
   validates :tipo,  :length => { :maximum => 1, :too_long  => "1 carattere obbligatorio (Valori ammessi: E = Entrata - U = Uscita - T = Trasferimento" }
+  validates :contabile, :length => { :maximum => 1, :too_long  => "1 carattere obbligatorio (Valori ammessi: S = Mov.Contabile - N = Mov.non Contabile" }
 end
--- test/unit/helpers/mags_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MagsHelperTest < ActionView::TestCase
-end
