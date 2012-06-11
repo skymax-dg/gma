c18e98b30ae694d3c518c50175647891d2095d34
unknown <Prod01@Prod01-HP.(none)>
2012-06-11 12:07:49 +0200

    modificato helper PDF, inserita classe generalePDF

--- app/controllers/cvendistitle_controller.rb
+++ app/controllers/cvendistitle_controller.rb
@@ -1,70 +1,71 @@
 class CvendistitleController < Ruport::Controller
-  stage :mov_header, :mov_body, :mov_footer
+  prepare :std_report
+  stage :company_header, :Cvendistitle_header, :Cvendistitle_body, :Cvendistitle_footer
+  finalize :std_report
+end
 
-  class PDF < Ruport::Formatter::PDF
-    include Helper_pdf
+include Helper_pdf
+class CvendistitlePDF < CompanyPDFBase
+  renders :pdf, :for => CvendistitleController
 
-    renders :pdf, :for => CvendistitleController
-    build :mov_header do
-      main_pdf_header # Metodo per intestazione std
-      options.text_format = { :font_size => 18, :justification => :center, :bold => true }
-      
-      add_text "VENDITE PER TITOLO E DISTRIBUTORE"
-      pad(20) { hr }
-      render_pdf
-    end
-
-    build :mov_body do
-      options.text_format = { :font_size => 14, :justification => :left }
-      Article.titcvend(data).each do |art|
-        desart = 'ARTICOLO: ' + art.codice + '    ' + art.descriz
-        add_text desart
-        Article.distit(art.id).each do |dis|
-          desdis = 'DISTRIBUTORE: ' + dis.codice + ' ' + dis.denomin
-          options.text_format = { :font_size => 14, :justification => :left } 
-          options.table_format = {:width => 530, :bold_headings => true}
-          table = buildtab(art.id, dis.id)
-          draw_table(table, :title => desdis, :title_font_size => 14, :protect_rows => 4,
-                     :column_options => {'Data_doc' => {:width => 80},
-                                         'Numero' => {:width => 60},
-                                         'Causale' => {:width => 250},
-                                         'Carico' => {:width => 50, :justification => :right},
-                                         'Vendite' => {:width => 50, :justification => :right},
-                                         'Giac.' => {:width => 50, :justification => :right}
-                                         })
-          pad(20) { hr }
-        end
-      end
-      render_pdf
-    end
-
-    build :mov_footer do
-      add_text " "
-      add_text "FINE della PAGINA (ALEX)"
-      render_pdf
-    end
-
-    def buildtab(art_id, dis_id)
-      table = Ruport::Data::Table.new
-      col_head = ["Data_doc", "Numero", "Causale", "Carico", "Vendite", "Giac."]
-      giac = 0
-      Article.vendtitdist(art_id, dis_id).each do |r|
-        dt_doc = r.attributes["data_doc"]
-        num = r.attributes["numero"]
-        cau = r.attributes["causale"]
-        mov = r.attributes["magcli"]
-        magint = r.attributes["movmag"]
-        qta = r.attributes["qta"]
-        magint == "M" and mov == "C" ? car=qta  : car=""
-        magint == "M" and mov == "S" ? car=-(qta.to_i) : car=car
-        magint != "M"  and mov == "S" ? sca=qta  : sca=""
-        magint != "M"  and mov == "C" ? sca=-(qta.to_i) : sca=sca
-        giac = giac + car.to_i - sca.to_i
-        table << Ruport::Data::Record.new([dt_doc, num, cau, car, sca, giac.to_s],
-                                          :attributes => col_head)
-      end
-      table.column_names = col_head
-      table
-    end  
+  build :Cvendistitle_header do
+    options.text_format = { :font_size => 18, :justification => :center, :bold => true }
+    
+    add_text "VENDITE PER TITOLO E DISTRIBUTORE"
+    pad(20) { hr }
+    render_pdf
   end
+
+  build :Cvendistitle_body do
+    options.text_format = { :font_size => 14, :justification => :left }
+    Article.titcvend(data).each do |art|
+      desart = 'ARTICOLO: ' + art.codice + '    ' + art.descriz
+      add_text desart
+      Article.distit(art.id).each do |dis|
+        desdis = 'DISTRIBUTORE: ' + dis.codice + ' ' + dis.denomin
+        options.text_format = { :font_size => 14, :justification => :left } 
+        options.table_format = {:width => 530, :bold_headings => true}
+        table = buildtab(art.id, dis.id)
+        draw_table(table, :title => desdis, :title_font_size => 14, :protect_rows => 4,
+                   :column_options => {'Data_doc' => {:width => 80},
+                                       'Numero' => {:width => 60},
+                                       'Causale' => {:width => 250},
+                                       'Carico' => {:width => 50, :justification => :right},
+                                       'Vendite' => {:width => 50, :justification => :right},
+                                       'Giac.' => {:width => 50, :justification => :right}
+                                      })
+        pad(20) { hr }
+      end
+    end
+    render_pdf
+  end
+
+  build :Cvendistitle_footer do
+    add_text " "
+    add_text "FINE della PAGINA (ALEX)"
+    render_pdf
+  end
+
+  def buildtab(art_id, dis_id)
+    table = Ruport::Data::Table.new
+    col_head = ["Data_doc", "Numero", "Causale", "Carico", "Vendite", "Giac."]
+    giac = 0
+    Article.vendtitdist(art_id, dis_id).each do |r|
+      dt_doc = r.attributes["data_doc"]
+      num = r.attributes["numero"]
+      cau = r.attributes["causale"]
+      mov = r.attributes["magcli"]
+      magint = r.attributes["movmag"]
+      qta = r.attributes["qta"]
+      magint == "M" and mov == "C" ? car=qta  : car=""
+      magint == "M" and mov == "S" ? car=-(qta.to_i) : car=car
+      magint != "M"  and mov == "S" ? sca=qta  : sca=""
+      magint != "M"  and mov == "C" ? sca=-(qta.to_i) : sca=sca
+      giac = giac + car.to_i - sca.to_i
+      table << Ruport::Data::Record.new([dt_doc, num, cau, car, sca, giac.to_s],
+                                        :attributes => col_head)
+    end
+    table.column_names = col_head
+    table
+  end  
 end
--- app/controllers/movtitle_controller.rb
+++ app/controllers/movtitle_controller.rb
@@ -1,75 +1,76 @@
 class MovtitleController < Ruport::Controller
-  stage :mov_header, :mov_body, :mov_footer
+  prepare :std_report
+  stage :company_header, :Movtitle_header, :Movtitle_body, :Movtitle_footer
+  finalize :std_report
+end
 
-  class PDF < Ruport::Formatter::PDF
-    include Helper_pdf
+include Helper_pdf
+class MovtitlePDF < CompanyPDFBase
+  renders :pdf, :for => MovtitleController
 
-    renders :pdf, :for => MovtitleController
-    build :mov_header do
-      main_pdf_header # Metodo per intestazione std
-      options.text_format = { :font_size => 18, :justification => :center, :bold => true }
-      
-      add_text "MOVIMENTAZIONI PER TITOLO"
-      pad(20) { hr }
-      render_pdf
-    end
-
-    build :mov_body do
-      options.text_format = { :font_size => 14, :justification => :left }
-      Article.movimentati(data).each do |art|
-        des = 'ARTICOLO: ' + art.codice + '    ' + art.descriz
-        options.table_format = {:width => 530, :bold_headings => true}
-        table = buildtab(art.id)
-        draw_table(table, :title => des, :title_font_size => 14, :protect_rows => 4,
-                   :column_options => {'Data_doc' => {:width => 80},
-                                       'Numero' => {:width => 60},
-                                       'Causale' => {:width => 210},
-                                       'Car.' => {:width => 40, :justification => :right},
-                                       'Scar.' => {:width => 40, :justification => :right},
-                                       'Giac.' => {:width => 40, :justification => :right},
-                                       'Impe.' => {:width => 40, :justification => :right},
-                                       'Disp.' => {:width => 40, :justification => :right}
-                                       })
-        pad(20) { hr }
-      end
-      render_pdf
-    end
-
-    build :mov_footer do
-      add_text " "
-      add_text "FINE della PAGINA (ALEX)"
-      render_pdf
-    end
-
-    def buildtab(id)
-      table = Ruport::Data::Table.new
-      col_head = ["Data_doc", "Numero", "Causale", "Car.", "Scar.", "Giac.", "Impe.", "Disp."]
-      giac = 0
-      tcar = 0
-      tsca = 0
-      timp = 0
-      Article.movmag(id).each do |r|
-        dt_doc = r.attributes["data_doc"]
-        num = r.attributes["numero"]
-        cau = r.attributes["causale"]
-        mov = r.attributes["mov"]
-        qta = r.attributes["qta"]
-        mag = r.attributes["mag"]
-        mov == "E" and mag == 'M' ? car=qta : car=""
-        mov == "U" and mag == 'M' ? sca=qta : sca=""
-        mov == "E" and mag == 'I' ? imp=-qta : imp=""
-        mov == "U" and mag == 'I' ? imp= qta : imp=""
-        giac += car.to_i - sca.to_i
-        tcar += car.to_i
-        tsca += sca.to_i
-        timp += imp.to_i
-        table << Ruport::Data::Record.new([dt_doc, num, cau, car, sca, giac.to_s, imp, giac-imp.to_i],
-                                          :attributes => col_head)
-      end
-      table << Ruport::Data::Record.new(["TOTALI:", "", "", tcar, tsca, giac, timp, giac-timp],
-                                          :attributes => col_head)
-      table.column_names = col_head
-      table
-    end  
+  build :Movtitle_header do
+    options.text_format = { :font_size => 18, :justification => :center, :bold => true }
+    
+    add_text "MOVIMENTAZIONI PER TITOLO"
+    pad(20) { hr }
+    render_pdf
   end
+
+  build :Movtitle_body do
+    options.text_format = { :font_size => 14, :justification => :left }
+    Article.movimentati(data).each do |art|
+      des = 'ARTICOLO: ' + art.codice + '    ' + art.descriz
+      options.table_format = {:width => 530, :bold_headings => true}
+      table = buildtab(art.id)
+      draw_table(table, :title => des, :title_font_size => 14, :protect_rows => 4,
+                 :column_options => {'Data_doc' => {:width => 80},
+                                     'Numero' => {:width => 60},
+                                     'Causale' => {:width => 210},
+                                     'Car.' => {:width => 40, :justification => :right},
+                                     'Scar.' => {:width => 40, :justification => :right},
+                                     'Giac.' => {:width => 40, :justification => :right},
+                                     'Impe.' => {:width => 40, :justification => :right},
+                                     'Disp.' => {:width => 40, :justification => :right}
+                                     })
+      pad(20) { hr }
+    end
+    render_pdf
+  end
+
+  build :Movtitle_footer do
+    add_text " "
+    add_text "FINE della PAGINA (ALEX)"
+    render_pdf
+  end
+
+  def buildtab(id)
+    table = Ruport::Data::Table.new
+    col_head = ["Data_doc", "Numero", "Causale", "Car.", "Scar.", "Giac.", "Impe.", "Disp."]
+    giac = 0
+    tcar = 0
+    tsca = 0
+    timp = 0
+    Article.movmag(id).each do |r|
+      dt_doc = r.attributes["data_doc"]
+      num = r.attributes["numero"]
+      cau = r.attributes["causale"]
+      mov = r.attributes["mov"]
+      qta = r.attributes["qta"]
+      mag = r.attributes["mag"]
+      mov == "E" and mag == 'M' ? car=qta : car=""
+      mov == "U" and mag == 'M' ? sca=qta : sca=""
+      mov == "E" and mag == 'I' ? imp=-qta : imp=""
+      mov == "U" and mag == 'I' ? imp= qta : imp=""
+      giac += car.to_i - sca.to_i
+      tcar += car.to_i
+      tsca += sca.to_i
+      timp += imp.to_i
+      table << Ruport::Data::Record.new([dt_doc, num, cau, car, sca, giac.to_s, imp, giac-imp.to_i],
+                                        :attributes => col_head)
+    end
+    table << Ruport::Data::Record.new(["TOTALI:", "", "", tcar, tsca, giac, timp, giac-timp],
+                                        :attributes => col_head)
+    table.column_names = col_head
+    table
+  end  
 end
