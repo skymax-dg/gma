fe13e2912a7747de90a150dd5bb62b8962f7cc4b
alex <alexzane@alice.it>
2012-10-30 15:31:43 +0100

    risolto bug in documenti se orario spedizione rimaneva null

--- app/controllers/spedizs_controller.rb
+++ app/controllers/spedizs_controller.rb
@@ -26,7 +26,11 @@ class SpedizsController < ApplicationController
   end
 
   def create
-    params[:spediz][:dtrit]=params[:spediz][:dtrit].to_my_date if params[:spediz][:dtrit]&&params[:spediz][:dtrit].is_date?
+    if params[:spediz][:dtrit]&&params[:spediz][:dtrit].is_date?
+      params[:spediz][:dtrit]=params[:spediz][:dtrit].to_my_date
+    else
+      params[:spediz][:dtrit]="00:00:00".to_my_date
+    end
     @spediz = Tesdoc.find(params[:spediz][:tesdoc_id]).build_spediz(params[:spediz])
     if @spediz.save
       flash[:success] = "Dati spedizione inseriti con successo."
@@ -40,7 +44,11 @@ class SpedizsController < ApplicationController
 
   def update
 # la riga seguente è da rivedere ed implementare un custom validate
-params[:spediz][:dtrit]=params[:spediz][:dtrit].to_my_date if params[:spediz][:dtrit]&&params[:spediz][:dtrit].is_date?
+    if params[:spediz][:dtrit]&&params[:spediz][:dtrit].is_date?
+      params[:spediz][:dtrit]=params[:spediz][:dtrit].to_my_date
+    else
+      params[:spediz][:dtrit]="00:00:00".to_my_date
+    end
     @spediz = Spediz.find(params[:id])
     if @spediz.update_attributes(params[:spediz])
       flash[:success] = "Dati spedizione aggiornati."
