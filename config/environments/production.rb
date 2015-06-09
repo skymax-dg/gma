8ed6fa6da6bc41187a64acedf6d269e83b439ea5
leonardo cigolini <l.cigolini@skymax-dg.com>
2015-06-09 14:59:03 +0200

    configurato per non usare gli assets precompilati modificato database .yml per nuovo server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,10 +12,10 @@ Gma::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # la riga seguente è stata messa a true da ALEX, prima era false
   #config.assets.compile = true
